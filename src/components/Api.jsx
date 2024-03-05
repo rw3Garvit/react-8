afb4c85f963b9cd6376d5a1f36269b069690b71c
Garvit Dhameliya <rw3.garvit.rd@gmail.com>
2024-03-05 09:05:06 +0530

    post req success

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,16 +1,33 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 
 const Api = () => {
   let [data, setdata] = useState([]);
+  let name = useRef()
+  let age = useRef()
 
   let fetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+    axios.get("http://localhost:3001/post").then((res) => {
       //   console.log(res.data);
       setdata(res.data);
     });
   };
+
+  let handleSubmit =()=>{
+    let dataa = {
+      name:name.current.value,
+      age:age.current.value
+    }
+
+  
+
+    axios.post('http://localhost:3001/post',dataa).then((res)=>{
+console.log(res.data);
+// fetchData()
+setdata([...data,res.data])
+    })
+  }
 
   //   console.log(data, "data");
 
@@ -19,13 +36,20 @@ const Api = () => {
   }, []);
 
   return (
-    <div class="row">
+
+    <>
+
+<input type="text" name="name" ref={name} />
+<input type="number" name="age"  ref={age}/>
+<button onClick={handleSubmit}>Add</button>
+
+<div class="row">
       {data?.map((val, ind) => {
         return (
           <div class="card" style={{ width: "18rem" }}>
             <div class="card-body">
-              <h5 class="card-title">{val.title}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">{val.id}</h6>
+              <h5 class="card-title">{val.name}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{val.age}</h6>
               <p class="card-text">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
@@ -38,6 +62,8 @@ const Api = () => {
         );
       })}
     </div>
+    </>
+  
   );
 };
 
