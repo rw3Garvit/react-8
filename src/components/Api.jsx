344808383127240522d33c9bfdf4738c58d6d061
Garvit Dhameliya <rw3.garvit.rd@gmail.com>
2024-03-07 08:40:13 +0530

    delete complate

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -4,9 +4,10 @@ import { useState } from "react";
 
 const Api = () => {
   let [data, setdata] = useState([]);
-  let name = useRef()
-  let age = useRef()
+  let name = useRef();
+  let age = useRef();
 
+  //get data
   let fetchData = () => {
     axios.get("http://localhost:3001/post").then((res) => {
       //   console.log(res.data);
@@ -14,56 +15,61 @@ const Api = () => {
     });
   };
 
-  let handleSubmit =()=>{
+  //add data
+  let handleSubmit = () => {
     let dataa = {
-      name:name.current.value,
-      age:age.current.value
-    }
+      name: name.current.value,
+      age: age.current.value,
+    };
 
-  
+    axios.post("http://localhost:3001/post", dataa).then((res) => {
+      console.log(res.data);
+      // fetchData()
+      setdata([...data, res.data]);
+    });
+  };
 
-    axios.post('http://localhost:3001/post',dataa).then((res)=>{
-console.log(res.data);
-// fetchData()
-setdata([...data,res.data])
-    })
-  }
+  //delete data
+  let deleteData = (id) => {
+    console.log(id);
 
-  //   console.log(data, "data");
+    axios.delete(`http://localhost:3001/post/${id}`).then((res) => {
+      console.log(res);
+      setdata(data.filter((val) => val.id !== id));
+    });
+  };
 
   useEffect(() => {
+    //get data
     fetchData();
   }, []);
 
   return (
-
     <>
+      <input type="text" name="name" ref={name} />
+      <input type="number" name="age" ref={age} />
+      <button onClick={handleSubmit}>Add</button>
 
-<input type="text" name="name" ref={name} />
-<input type="number" name="age"  ref={age}/>
-<button onClick={handleSubmit}>Add</button>
-
-<div class="row">
-      {data?.map((val, ind) => {
-        return (
-          <div class="card" style={{ width: "18rem" }}>
-            <div class="card-body">
-              <h5 class="card-title">{val.name}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">{val.age}</h6>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <a href="#" class="card-link">
-                view
-              </a>
+      <div class="row">
+        {data?.map((val, ind) => {
+          return (
+            <div class="card" style={{ width: "18rem" }}>
+              <div class="card-body">
+                <h5 class="card-title">{val.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{val.age}</h6>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <button onClick={() => deleteData(val.id)} class="card-link">
+                  Delete
+                </button>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
     </>
-  
   );
 };
 
